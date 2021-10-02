36d73e86c9270937b213255c53b1d2d9b0897c79
dabideee13 <d.e.magno13@gmail.com>
2021-10-02 15:33:43 +0800

    Update: first working version of ui

--- src/ui.py
+++ src/ui.py
@@ -2,77 +2,66 @@ from typing import Any
 
 from tkinter import Tk, Label, Button, Entry, StringVar
 
+from core import SampleSizeCalculator, SampleSizeParameter
 
-class Field:
+app = Tk()
+app.title("Sample Size Calculator")
+app.geometry('500x300')
+app.resizable(False, False)
 
-    def __init__(self, app: Tk) -> None:
-        self.app = app
+total_population_label = Label(app, text="Total population: ")
+total_population_label.place(x=10, y=20)
+total_population_label.pack()
 
-    @property
-    def total_population(self) -> None:
-        return _entry(x=210, y=20)
+total_population_entry = Entry(app, textvariable=StringVar())
+total_population_entry.place(x=210, y=20)
+total_population_entry.pack()
 
-    @property
-    def confidence_level(self) -> None:
-        return _entry(x=210, y=90)
+confidence_level_label = Label(app, text="Confidence level: ")
+confidence_level_label.place(x=10, y=90)
+confidence_level_label.pack()
 
-    @property
-    def margin_of_error(self) -> None:
-        return _entry(x=210, y=160)
+confidence_level_entry = Entry(app, textvariable=StringVar())
+confidence_level_entry.place(x=210, y=90)
+confidence_level_entry.pack()
 
-    @property
-    def target_proportion(self) -> None:
-        return _entry(x=210, y=230)
+margin_of_error_label = Label(app, text="Margin of error: ")
+margin_of_error_label.place(x=10, y=160)
+margin_of_error_label.pack()
 
-    @property
-    def add_name(text: str) -> None:
-        return Label(self.app, text=text)
+margin_of_error_entry = Entry(app, textvariable=StringVar())
+margin_of_error_entry.place(x=210, y=160)
+margin_of_error_entry.pack()
 
-    def _entry(self, text: str):
-        return Entry(self.app, textvariable=StringVar()).place(x=x, y=y)
+target_proportion_label = Label(app, text="Target proportion: ")
+target_proportion_label.place(x=10, y=230)
+target_proportion_label.pack()
 
+target_proportion_entry = Entry(app, textvariable=StringVar())
+target_proportion_entry.place(x=210, y=230)
+target_proportion_entry.pack()
 
-class App:
-
-    def __init__(self, app: Tk, sample_size: int, **kwargs: Any) -> None:
-        self.app = app()
-        self.sample_size = sample_size
-        self.kwargs = kwargs
-
-    def entry_total_population(self) -> None:
-        self.entry = Entry(self.app)
-        self.entry.pack()
-
-    def label_calculate(self, *args: Any) -> None:
-        self.label = Label(self.app, text=f'Total sample size: {self.entry.get()}')
-        self.label.pack()
-
-    def button_calculate(self) -> None:
-        self.button = Button(self.app, text="Calculate", command=self.label_calculate)
-        self.button.pack()
-
-    def run(self) -> None:
-        self.setup()
-        self.entry_total_population()
-        self.button_calculate()
-        self.app.bind('<Return>', self.label_calculate)
-        self.app.mainloop()
-
-    def setup(self) -> None:
-        if 'title' in list(self.kwargs.keys()):
-            self.app.title(self.kwargs['title'])
-        self.app.geometry('500x300')
-        self.app.resizable(False, False)
-
-    # TODO: Handle string inputs here when user mistakenly inputs alphabets and special characters
-    # TODO: Add output for inputs
-    # TODO: Add logger
+calculator = SampleSizeCalculator()
 
 
-def main():
-    app = App(Tk, 383, title="Sample Size Calculator")
-    app.run()
+def calculate(*args: Any) -> None:
+    calculation_label = Label(
+        app,
+        text=(
+            "Total sample size: "
+            f"{calculator.calculate(int(total_population_entry.get()), float(confidence_level_entry.get()), float(margin_of_error_entry.get()), float(target_proportion_entry.get()))}"
+        )
+    )
+    calculation_label.place(x=10, y=320)
+    calculation_label.pack()
 
 
-if __name__ == '__main__':
-    main()
+def button() -> None:
+    calculation_button = Button(app, text="Calculate", command=calculate)
+    calculation_button.place(x=10, y=300)
+    calculation_button.pack()
+
+
+button()
+app.bind('<Return>', calculate)
+app.mainloop()
